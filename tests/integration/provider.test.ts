c2fbb45b111458b1c2809870c4c87ea900cec70c
Paul Cheng <chengyuejia@foxmail.com>
2024-03-24 21:15:16 +0800

    bugfix

--- tests/integration/provider.test.ts
+++ tests/integration/provider.test.ts
@@ -8,7 +8,7 @@ describe("Provider", () => {
     const PRIVATE_KEY = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
     const RECEIVER = "0xa61464658AfeAf65CccaaFD3a512b69A83B77618";
 
-    const provider = Provider.getDefaultProvider(types.Network.Localhost);
+    const provider = Provider.getDefaultProvider();
     const wallet = new Wallet(PRIVATE_KEY, provider);
 
     let tx = null;
